649c8d534b40bac79a7d36a73336a8dceb31de08
Justin Bellars <jbellars@gmail.com>
2024-11-26 15:35:28 -0700

    Add type annotations

--- src/main/scala/net/degoes/ThinkingFunctionally.scala
+++ src/main/scala/net/degoes/ThinkingFunctionally.scala
@@ -9,44 +9,44 @@ import Common._
 import scalaz.zio.stream.ZSink.fold
 
 /**
- * Procedural effects *do*; functional effects *describe*.
- *
- * Procedural effect:
- *
- * {{{
- *   def println(line: String): Unit
- * }}}
- *
- * Functional effect:
- *
- * {{{
- *   def println(line: String): Task[Unit]
- * }}}
- *
- * where, for example, `case class Task[A](unsafeRun: () => A)`
- *
- * ZIO's only functional effect type:
- *
- * {{{
- *    ZIO[-R, +E, +A]
- * }}}
- *
- * Similar to a purely functional, but effectful version of this function:
- *
- * {{{
- *    R => Either[E, A]
- * }}}
- *
- * ZIO has type aliases to simplify common use cases:
- *
- * {{{
- *    type   UIO[+A] = ZIO[Any,   Nothing, A]
- *    type  Task[+A] = ZIO[Any, Throwable, A]
- *
- *    type TaskR[-R, +A] = ZIO[  R, Throwable, A]
- *    type    IO[+E, +A] = ZIO[Any,         E, A]
- * }}}
- */
+  * Procedural effects *do*; functional effects *describe*.
+  *
+  * Procedural effect:
+  *
+  * {{{
+  *   def println(line: String): Unit
+  * }}}
+  *
+  * Functional effect:
+  *
+  * {{{
+  *   def println(line: String): Task[Unit]
+  * }}}
+  *
+  * where, for example, `case class Task[A](unsafeRun: () => A)`
+  *
+  * ZIO's only functional effect type:
+  *
+  * {{{
+  *    ZIO[-R, +E, +A]
+  * }}}
+  *
+  * Similar to a purely functional, but effectful version of this function:
+  *
+  * {{{
+  *    R => Either[E, A]
+  * }}}
+  *
+  * ZIO has type aliases to simplify common use cases:
+  *
+  * {{{
+  *    type   UIO[+A] = ZIO[Any,   Nothing, A]
+  *    type  Task[+A] = ZIO[Any, Throwable, A]
+  *
+  *    type TaskR[-R, +A] = ZIO[  R, Throwable, A]
+  *    type    IO[+E, +A] = ZIO[Any,         E, A]
+  * }}}
+  */
 object ThinkingFunctionally extends App {
   import functional._
   import logging._
@@ -54,30 +54,30 @@ object ThinkingFunctionally extends App {
   import auth._
   import email._
 
-  def run(args: List[String]) = putStrLn("Hello World!").fold(_ => 1, _ => 0)
+  def run(args: List[String]): ZIO[Console, Nothing, Int] = putStrLn("Hello World!").fold(_ => 1, _ => 0)
 
   type Services = Auth with Logging with Social with Clock with Email with Blocking
 
   /**
-   * Your mission, should you choose to accept it:
-   *
-   * 1. Refactor the procedural/OOP code to its functional equivalent.
-   * 2. Fix the two cases of thread pool exhaustion.
-   * 4. Ensure errors that happen here always propagate upward.
-   * 5. Locally rate limit the social / email services to prevent overload.
-   *
-   * Questions for Thought:
-   *
-   *   1. Which issues were fixed automatically? Which required attention?
-   *   2. For those that required attention to fix, how are the solutions
-   *      an improvement over the same solutions in the procedural version?
-   *   3. How easy is will it be to change the functional version, versus
-   *      the procedural version?
-   *   4. How many styles of computation are there in the procedural version?
-   *      How many in the functional version?
-   *   5. How much logic is devoted to error handling in the procedural version?
-   *      How much in the functional version?
-   */
+    * Your mission, should you choose to accept it:
+    *
+    * 1. Refactor the procedural/OOP code to its functional equivalent.
+    * 2. Fix the two cases of thread pool exhaustion.
+    * 4. Ensure errors that happen here always propagate upward.
+    * 5. Locally rate limit the social / email services to prevent overload.
+    *
+    * Questions for Thought:
+    *
+    *   1. Which issues were fixed automatically? Which required attention?
+    *   2. For those that required attention to fix, how are the solutions
+    *      an improvement over the same solutions in the procedural version?
+    *   3. How easy is will it be to change the functional version, versus
+    *      the procedural version?
+    *   4. How many styles of computation are there in the procedural version?
+    *      How many in the functional version?
+    *   5. How much logic is devoted to error handling in the procedural version?
+    *      How much in the functional version?
+    */
   def inviteFriends(token: AuthToken): ZIO[Services, Throwable, Receipt] =
     for {
       userId      <- auth.login(token)
@@ -100,21 +100,21 @@ object ThinkingFunctionally extends App {
     } yield receipts.foldLeft(Receipt.empty)(_ |+| _) // Loss-lessly capturing 100 percent of the errors.
 
   // Superpower #1: Composable timeouts
-  lazy val invitation1 = inviteFriends(???).timeout(60.seconds)
+  lazy val invitation1: ZIO[Services with Clock, Throwable, Option[Receipt]] = inviteFriends(???).timeout(60.seconds)
 
   // Superpower #2: Lossless & localized errors w/auto propagation:
   invitation1.flatMap(receipt => putStrLn(s"Receipt is $receipt"))
 
   /**
-   * Describes the result of an email invitation. Either it failed with some
-   * errors, or succeeded.
-   */
+    * Describes the result of an email invitation. Either it failed with some
+    * errors, or succeeded.
+    */
   case class Receipt(value: Map[UserID, Option[Throwable]]) { self =>
 
     final def |+|(that: Receipt): Receipt =
       Receipt(self.value ++ that.value)
 
-    final def succeeded: Int            = value.values.filter(_ == None).size
+    final def succeeded: Int            = value.values.count(_.isEmpty)
     final def failures: List[Throwable] = value.values.collect { case Some(t) => t }.toList
   }
 
@@ -141,7 +141,7 @@ object functional {
 
       trait Live extends Logging {
 
-        val logging = new Service {
+        val logging: Service = new Service {
           // Implement this using Common.log:
           // Hint: UIO.effectTotal
           def log(message: String): UIO[Unit] = ???
@@ -168,7 +168,7 @@ object functional {
 
       trait Live extends Auth with Blocking {
 
-        val auth = new Service {
+        val auth: Service = new Service {
 
           // Implement this using Common.login:
           // Hint: blocking.interruptible
@@ -198,7 +198,7 @@ object functional {
 
       trait Live extends Social {
 
-        val social = new Service {
+        val social: Service = new Service {
 
           // Implement this using Common.getProfile:
           // Hint: ZIO.effectAsync
@@ -237,7 +237,7 @@ object functional {
 
       trait Live extends Email with Blocking {
 
-        val email = new Service {
+        val email: Service = new Service {
 
           // Implement this using Common.sendEmail:
           // Hint: blocking.interruptible combinator
